547ad1c0da7adf0e367c221006a04d7c7b6f0747
Débora Albornoz <debsalbornoz@gmail.com>
2024-12-24 12:46:41 -0500

    ex02

---
+++ module01/ex02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void)
+{
+    std::string str="HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string& stringREF = str;
+
+    std::cout << "Memory address of the string variable: " << &str << "\n";
+    std::cout << "Memory address held by stringPTR: " << stringPTR << "\n";
+    std::cout << "Memory adress held by stringREF: " << &stringREF << "\n";
+
+    std::cout << "The value of the string variable: " << str << "\n";
+    std::cout << "The value pointed by stringPTR: " << *stringPTR << "\n";
+    std::cout << "The value pointed by stringREF: " << stringREF << "\n";
+}
